85c9a0698988df08e90e32e658c7983b25f9fb52
Nechami0517 <nechami0517@gmail.com>
2025-02-18 18:24:08 +0200

    lesson2-runing

--- Models/Book .cs
+++ Models/Book .cs
@@ -1,12 +1,14 @@
+using Microsoft.OpenApi.Models;
 namespace project;
+
 
 public class Book
 {
     public int Id { get; set; }
 
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
-    public string  Auther{get; set;}
+    public string?  Auther{get; set;}
 
     public double Price { get; set; }
     public DateOnly Date { get; set; }
--- Services/BookService.cs
+++ Services/BookService.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+
+
 using project.Models;
 
 namespace project.Services;
